746697cda75d833f90355448b5c2f67e660dc027
WaiEnu <strayhituji@gmail.com>
2023-05-19 17:10:08 +0900

    230519

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
-
 import vue from "@astrojs/vue";
+
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue()]
+  integrations: [vue(), image()]
 });
