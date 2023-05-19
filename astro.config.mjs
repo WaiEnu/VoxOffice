f08a5bcc79fd7ba593b732197917374a08fb3426
WaiEnu <strayhituji@gmail.com>
2023-05-19 17:33:11 +0900

    230519

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue(), image()]
+  integrations: [vue(), image()],
+  site: 'https://WaiEnu.github.io',
+  base: '/VoxOffice',
 });
